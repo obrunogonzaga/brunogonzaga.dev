6291e041da49691d3de33a7208b0f8740f5e6455
Bruno Gonzaga Santos <brunog86@gmail.com>
2021-01-22 21:20:40 +0000

    Remoção do ul and li

--- src/patterns/components/Nav/index.js
+++ src/patterns/components/Nav/index.js
@@ -8,40 +8,23 @@ const links = [
 function Nav() {
   return (
     <nav>
-      <ul className="flex items-center justify-between p-8">
-        <div className="flex justify-start">
-          <li>
-            <Link href="/">
-              <a className="mx-2 text-blue-500 font-bold no-underline text-accent-1 dark:text-blue-300">
-                Bruno Gonzaga
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a className="mx-2 text-blue-500 no-underline text-accent-1 dark:text-blue-300">
-                Artigos
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a className="mx-2 text-blue-500 no-underline text-accent-1 dark:text-blue-300">
-                Sobre
-              </a>
-            </Link>
-          </li>
-        </div>
-        <ul className="flex items-center justify-between space-x-4">
-          {links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <a href={href} className="no-underline btn-blue">
-                {label}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </ul>
+      <div>
+        <Link href="/">
+          <a className="mx-2 text-blue-500 font-bold no-underline text-accent-1 dark:text-blue-300">
+            Bruno Gonzaga
+          </a>
+        </Link>
+        <Link href="/">
+          <a className="mx-2 text-blue-500 no-underline text-accent-1 dark:text-blue-300">
+            Artigos
+          </a>
+        </Link>
+        <Link href="/">
+          <a className="mx-2 text-blue-500 no-underline text-accent-1 dark:text-blue-300">
+            Sobre
+          </a>
+        </Link>
+      </div>
     </nav>
   );
 }
